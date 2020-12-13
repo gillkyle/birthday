4ce91009c1ec0d374942b4a25752bca190635587
Kyle Gill <kylerobertgill@gmail.com>
2020-12-13 12:18:33 -0700

    flip order of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,11 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/*`] },
     },
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
